e8bac4b0cc985e6ed0a6c1295b70c2857283610e
Elogeek <elodiechristin@gmail.com>
2021-02-08 09:45:22 +0100

    finish php exo 175

--- admin.php
+++ admin.php
@@ -5,12 +5,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Connected</title>
+    <title>Secret</title>
     <link rel="stylesheet" href="./style.css">
 </head>
 <body>
     <div id="container">
-        <a href="./index.php?deco" title="Déconnexion">Se déconnecter</a>
         <h1>Bienvenue sur la page secrète</h1>
         <p>Ceci est un petit paragraphe secret!</p>
         <p> un petit texte maintenant visible !</p>
